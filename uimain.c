9084795661833a8d00b9ca4d359cfd99148b7ce8
student <azavalaana@miners.utep.edu>
2021-09-12 22:01:19 -0600

    Added a different font to draw files and ui main in order to show them

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Vertical Arrow = v, Horizontal Arrow = h, Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print \nVertical Arrow = v, Horizontal Arrow = h, Triangle = t, \nSquare = s, Chars11x16 = c, Chars8x12 = 8 \nor 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -33,9 +33,14 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
+      puts("You selected 11x16 chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case '8':
+      puts("You selected 8x12 chars:");
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_8x12(c);
       break;
     case 'q':
       puts("Bye!");
