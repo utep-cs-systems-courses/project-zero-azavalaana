875c18b34c5d590e462f17e76d09fa4cb94e062d
student <azavalaana@miners.utep.edu>
2021-09-14 09:34:17 -0600

    Fixed Rotation of font

--- draw_chars.c
+++ draw_chars.c
@@ -16,12 +16,12 @@ void print_char_11x16(char c)
 void print_char_8x12(char c)
 {
     c -= 0x20;
-  for (char row = 0; row < 11; row++) {
-    unsigned short rowBits = font_8x12[c][row];
-    for (char col = 0; col < 16; col++) {
-      unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+    for (char col = 0; col < 8; col++) {
+      for (char row = 11; row >= 0; row--) {
+	unsigned short rowBits = font_8x12[c][row];
+	unsigned short colMask = 1 << (7-col); /* mask to select bit associated with bit */
+	putchar( (rowBits & colMask) ? '*' : ' ');
+      }
+      putchar('\n');
     }
-    putchar('\n');
-  }
 }
